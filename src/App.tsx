2638c0fa328b15b3ce0f085ae3087360b0e1a353
Sudhirkumar Kuchara <109846483+Sudhirkumar6009@users.noreply.github.com>
2025-05-26 19:30:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { AuthProvider } from "./contexts/AuthContext";
-import { Web3Provider } from "./contexts/Web3Context";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Index from "./pages/Index";
@@ -14,37 +13,47 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
+import { Web3Provider } from "./contexts/Web3Context";
+import { WagmiProvider, createConfig, http } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
 
 const queryClient = new QueryClient();
-
+const wagmiConfig = createConfig({
+  chains: [mainnet, sepolia], // add your chains here
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+});
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <ThemeProvider>
-        <AuthProvider>
+   <QueryClientProvider client={queryClient}>
+    <WagmiProvider config={wagmiConfig}>
+      <TooltipProvider>
+        <ThemeProvider>
           <Web3Provider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-              <div className="min-h-screen flex flex-col">
-                <Navbar />
-                {/* Add padding-top to account for fixed navbar */}
-                <main className="flex-1 pt-16">
-                  <Routes>
-                    <Route path="/" element={<Index />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="*" element={<NotFound />} />
-                  </Routes>
-                </main>
-                <Footer />
-              </div>
-            </BrowserRouter>
+            <AuthProvider>
+              <Toaster />
+              <Sonner />
+              <BrowserRouter>
+                <div className="min-h-screen flex flex-col">
+                  <Navbar />
+                  <main className="flex-1 pt-16">
+                    <Routes>
+                      <Route path="/" element={<Index />} />
+                      <Route path="/login" element={<Login />} />
+                      <Route path="/signup" element={<Signup />} />
+                      <Route path="/dashboard" element={<Dashboard />} />
+                      <Route path="*" element={<NotFound />} />
+                    </Routes>
+                  </main>
+                  <Footer />
+                </div>
+              </BrowserRouter>
+             </AuthProvider>
           </Web3Provider>
-        </AuthProvider>
-      </ThemeProvider>
-    </TooltipProvider>
+        </ThemeProvider>
+      </TooltipProvider>
+    </WagmiProvider>
   </QueryClientProvider>
 );
 
